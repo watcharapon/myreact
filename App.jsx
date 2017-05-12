c09bbec1149b7159a9f735edc2e5f99ad8103620
watcha.h <watcha.h@almacom.co.th>
2017-05-13 01:40:01 +0700

    test#2

--- App.jsx
+++ App.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 
 class App extends React.Component {
    render() {
+      var myStyle={
+        color: "red",
+        fontSize: 100,
+        backgroundColor: "yellow"
+      }
       return (
          <div>
-            Hello World Watcha!
+            <h1>Hello World Watcha! </h1>
+            <h2>Body</h2>
+            <h3 style={myStyle}>{1==2 ? "OK" : "No"}</h3>
          </div>
       );
    }
